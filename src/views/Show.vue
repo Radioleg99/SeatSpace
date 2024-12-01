733bec07be6fc591b5a3f04c4aefe17cb0a2b015
luchang <chang.lu@aalto.fi>
2024-12-01 23:35:38 +0200

    fix show basic info star not showing

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -94,10 +94,6 @@ onMounted(async () => {
     showBasicData.value = res
   })
 
-  //   getShowComments(route.params.id).then((res) => {
-  //     showComments.value = res
-  //   })
-
   loadshowCommentsList()
 })
 
@@ -386,406 +382,404 @@ function renderSeatCanvas(hallLayoutData) {
 </script>
 
 <template>
-	<!-- Control -->
-	<BackButton class="backBtn" />
-	<div class="tabBar" @click="switchPage" id="tabBar">
-		<div class="tabBarStatusBar" id="tabStatusBar" :style="{ left: isLayoutPage ? 0 : '124px' }"></div>
-		<div class="tabBarSeatText" id="tabSeat" :style="{ color: isLayoutPage ? 'white' : '#6E6E6E' }">SEAT</div>
-		<div class="tabBarShowText" id="tabShow" :style="{ color: isLayoutPage ? '#6E6E6E' : 'white' }">SHOW</div>
-	</div>
+  <!-- Control -->
+  <BackButton class="backBtn" />
+  <div class="tabBar" @click="switchPage" id="tabBar">
+    <div class="tabBarStatusBar" id="tabStatusBar" :style="{ left: isLayoutPage ? 0 : '124px' }"></div>
+    <div class="tabBarSeatText" id="tabSeat" :style="{ color: isLayoutPage ? 'white' : '#6E6E6E' }">SEAT</div>
+    <div class="tabBarShowText" id="tabShow" :style="{ color: isLayoutPage ? '#6E6E6E' : 'white' }">SHOW</div>
+  </div>
 
-	<!-- Seat Comments pop up -->
-	<div v-if="isCommentPop">
-		<!-- mask -->
-		<div class="darkMask"></div>
+  <!-- Seat Comments pop up -->
+  <div v-if="isCommentPop">
+    <!-- mask -->
+    <div class="darkMask"></div>
 
-		<!-- close and add comment button -->
-		<div class="btnContainer" style="left: calc(10% + 12px)" @click="closeCommentPop">
-			<img src="../assets/close-btn.svg" alt="close" class="btnIcon" />
-		</div>
-		<div class="btnContainer" style="right: calc(10% + 12px)" @click="jumpToCommentPage">
-			<img src="../assets/new-comment-btn.svg" class="btnIcon" />
-		</div>
+    <!-- close and add comment button -->
+    <div class="btnContainer" style="left: calc(10% + 12px)" @click="closeCommentPop">
+      <img src="../assets/close-btn.svg" alt="close" class="btnIcon" />
+    </div>
+    <div class="btnContainer" style="right: calc(10% + 12px)" @click="jumpToCommentPage">
+      <img src="../assets/new-comment-btn.svg" class="btnIcon" />
+    </div>
 
-		<!-- comments container -->
-		<div class="seatCommentPopContainer">
-			<div class="singleCommentContainer" v-for="comment in commentsList">
-				<RatingStars :rating="comment.rating" class="ratingStars" size="19.5" />
-				<div class="imgMask"></div>
-				<img :src="comment.imgUrl" alt="star" class="commentImg" />
-			</div>
+    <!-- comments container -->
+    <div class="seatCommentPopContainer">
+      <div class="singleCommentContainer" v-for="comment in commentsList">
+        <RatingStars :rating="comment.rating" class="ratingStars" size="19.5" />
+        <div class="imgMask"></div>
+        <img :src="comment.imgUrl" alt="star" class="commentImg" />
+      </div>
 
-			<div v-if="commentsList.length === 0" style="display: flex; width: 100%; height: 100%; justify-content: center; align-items: center; color: aliceblue">No comment found! Please add one!</div>
-		</div>
-	</div>
+      <div v-if="commentsList.length === 0"
+        style="display: flex; width: 100%; height: 100%; justify-content: center; align-items: center; color: aliceblue">
+        No comment found! Please add one!</div>
+    </div>
+  </div>
 
-	<!-- Seat Layout -->
-	<div v-show="isLayoutPage" id="seatCanvasContainer" class="layoutBg"></div>
+  <!-- Seat Layout -->
+  <div v-show="isLayoutPage" id="seatCanvasContainer" class="layoutBg"></div>
 
-	<!-- Show Detail -->
-	<div v-show="!isLayoutPage" class="showBg">
-		<!-- show basic info -->
-		<div v-if="showBasicData" class="showBasicInfoContainer">
-			<img :src="showBasicData.imgUrl" alt="showPoster" class="poster" />
-			<div class="infoBox">
-				<div class="showName">{{ showBasicData.showName }}</div>
-				<div class="showTheater">
-					<img src="../assets/map-pin.svg" />
-					<span> {{ showBasicData.theater + ' ' + showBasicData.hall }}</span>
-				</div>
-				<div class="showTheater">
-					<img src="../assets/clock.svg" />
-					<span>
-						{{ showBasicData.startTime }}
-					</span>
-				</div>
-				<RatingStars :rating="showBasicData.rating" size="8.5" />
-			</div>
-		</div>
+  <!-- Show Detail -->
+  <div v-show="!isLayoutPage" class="showBg">
+    <!-- show basic info -->
+    <div v-if="showBasicData" class="showBasicInfoContainer">
+      <img :src="showBasicData.imgUrl" alt="showPoster" class="poster" />
+      <div class="infoBox">
+        <div class="showName">{{ showBasicData.showName }}</div>
+        <div class="showTheater">
+          <img src="../assets/map-pin.svg" />
+          <span> {{ showBasicData.theater + ' ' + showBasicData.hall }}</span>
+        </div>
+        <div class="showTheater">
+          <img src="../assets/clock.svg" />
+          <span>
+            {{ showBasicData.startTime }}
+          </span>
+        </div>
+        <RatingStars v-if="showBasicData.rating" :rating="showBasicData.rating" size="8.5" />
+      </div>
+    </div>
 
-		<!-- show comments -->
-		<div class="showCommentsChangeBox" ref="containerRef" @scroll="handleScroll">
-			<div v-for="comment in showComments" class="commentBox">
-				<div class="commentImgBox">
-					<img v-for="img in comment.imgUrl" :src="img" alt="commentImg" class="commentImg" />
-				</div>
-				<div class="commentText">{{ comment.comment }}</div>
-				<div class="commentStar">
-					<RatingStars :rating="parseFloat(comment.rating)" size="10.5" />
-					<div class="commentDate">{{ comment.date }}</div>
-				</div>
-			</div>
-			<div v-if="isLoading && !isEnd" class="loading-indicator">
-				<img src="../assets/icon/refresh.svg" alt="Loading..." class="rotating-icon" />
-			</div>
-			<div v-if="isEnd" class="end-indicator">No more results</div>
-		</div>
+    <!-- show comments -->
+    <div class="showCommentsChangeBox" ref="containerRef" @scroll="handleScroll">
+      <div v-for="comment in showComments" class="commentBox">
+        <div class="commentImgBox">
+          <img v-for="img in comment.imgUrl" :src="img" alt="commentImg" class="commentImg" />
+        </div>
+        <div class="commentText">{{ comment.comment }}</div>
+        <div class="commentStar">
+          <RatingStars :rating="parseFloat(comment.rating)" size="10.5" />
+          <div class="commentDate">{{ comment.date }}</div>
+        </div>
+      </div>
+      <div v-if="isLoading && !isEnd" class="loading-indicator">
+        <img src="../assets/icon/refresh.svg" alt="Loading..." class="rotating-icon" />
+      </div>
+      <div v-if="isEnd" class="end-indicator">No more results</div>
+    </div>
 
-		<!-- poster background -->
-		<div
-			v-if="showBasicData"
-			class="showBgImg"
-			:style="{
-				background: `linear-gradient(180deg, rgba(0, 0, 0, 0.42) 24.5%, #505E2C 86.5%), url(${showBasicData.imgUrl}) lightgray 50% / cover no-repeat`,
-			}"
-		></div>
-	</div>
+    <!-- poster background -->
+    <div v-if="showBasicData" class="showBgImg" :style="{
+      background: `linear-gradient(180deg, rgba(0, 0, 0, 0.42) 24.5%, #505E2C 86.5%), url(${showBasicData.imgUrl}) lightgray 50% / cover no-repeat`,
+    }"></div>
+  </div>
 </template>
 
 <style scoped>
 .layoutBg {
-	background-color: #1a1a24;
-	z-index: 1;
-	overflow: hidden;
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
+  background-color: #1a1a24;
+  z-index: 1;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
 .backBtn {
-	position: fixed;
-	top: 40px;
-	left: 30px;
-	z-index: 2;
+  position: fixed;
+  top: 40px;
+  left: 30px;
+  z-index: 2;
 }
 
 .tabBar {
-	position: fixed;
-	top: 40px;
-	right: 30px;
-	width: 255px;
-	height: 35px;
-	background-color: #e9ebea;
-	z-index: 2;
-	border-radius: 50px;
+  position: fixed;
+  top: 40px;
+  right: 30px;
+  width: 255px;
+  height: 35px;
+  background-color: #e9ebea;
+  z-index: 2;
+  border-radius: 50px;
 }
 
 .tabBarSeatText {
-	position: absolute;
-	top: 50%;
-	transform: translateY(-50%);
-	left: 48px;
-	font-size: 14px;
-	font-weight: 700;
-	transition: all 0.3s;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 48px;
+  font-size: 14px;
+  font-weight: 700;
+  transition: all 0.3s;
 }
 
 .tabBarShowText {
-	position: absolute;
-	top: 50%;
-	transform: translateY(-50%);
-	right: 39px;
-	font-size: 14px;
-	font-weight: 700;
-	transition: all 0.3s;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 39px;
+  font-size: 14px;
+  font-weight: 700;
+  transition: all 0.3s;
 }
 
 .tabBarStatusBar {
-	position: absolute;
-	top: 50%;
-	transform: translateY(-50%);
-	width: 132px;
-	height: 36px;
-	background-color: #6f00ff;
-	border-radius: 50px;
-	transition: all 0.3s;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 132px;
+  height: 36px;
+  background-color: #6f00ff;
+  border-radius: 50px;
+  transition: all 0.3s;
 }
 
 .seatCommentPopContainer {
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	width: 80%;
-	height: 80%;
-	z-index: 4;
-	display: flex;
-	flex-direction: column;
-	gap: 16px;
-	color: #1a1a24;
-	padding: 8px;
-	border-radius: 15px;
-	border: 1px solid #4747bf;
-	background: #2f2f42;
-	box-shadow: 0px 0px 20px 0px #5b5bc6;
-	overflow-x: hidden;
-	overflow-y: scroll;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 80%;
+  z-index: 4;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  color: #1a1a24;
+  padding: 8px;
+  border-radius: 15px;
+  border: 1px solid #4747bf;
+  background: #2f2f42;
+  box-shadow: 0px 0px 20px 0px #5b5bc6;
+  overflow-x: hidden;
+  overflow-y: scroll;
 }
 
 .darkMask {
-	background: rgba(0, 0, 0, 0.65);
-	width: 100%;
-	height: 100%;
-	position: fixed;
-	z-index: 3;
-	top: 0;
-	left: 0;
+  background: rgba(0, 0, 0, 0.65);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 3;
+  top: 0;
+  left: 0;
 }
 
 .singleCommentContainer {
-	height: 191px;
-	position: relative;
+  height: 191px;
+  position: relative;
 }
 
 .singleCommentContainer .commentImg {
-	height: 191px;
-	width: 100%;
-	border-radius: 10px;
-	/* position: absolute; */
+  height: 191px;
+  width: 100%;
+  border-radius: 10px;
+  /* position: absolute; */
 }
 
 .ratingStars {
-	position: absolute;
-	left: 9px;
-	bottom: 11.5px;
-	z-index: 4;
+  position: absolute;
+  left: 9px;
+  bottom: 11.5px;
+  z-index: 4;
 }
 
 .imgMask {
-	border-radius: 0px 0px 10px 10px;
-	background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #1a1a24 100%);
-	position: absolute;
-	width: 100%;
-	height: 38px;
-	bottom: 0;
+  border-radius: 0px 0px 10px 10px;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #1a1a24 100%);
+  position: absolute;
+  width: 100%;
+  height: 38px;
+  bottom: 0;
 }
 
 .btnContainer {
-	position: fixed;
-	bottom: calc(10% + 20px);
-	z-index: 5;
-	width: 70px;
-	height: 70px;
-	flex-shrink: 0;
-	border-radius: 70px;
-	background: var(--active, #4747bf);
-	box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  position: fixed;
+  bottom: calc(10% + 20px);
+  z-index: 5;
+  width: 70px;
+  height: 70px;
+  flex-shrink: 0;
+  border-radius: 70px;
+  background: var(--active, #4747bf);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .btnIcon {
-	width: 24px;
-	height: 24px;
-	flex-shrink: 0;
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
 }
 
 .showBg {
-	position: fixed;
-	top: 0;
-	left: 0;
-	height: 100%;
-	width: calc(100% - 60px);
-	padding: 0 30px;
-	background-color: #505e2c;
-	z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: calc(100% - 60px);
+  padding: 0 30px;
+  background-color: #505e2c;
+  z-index: 1;
 
-	display: flex;
-	flex-direction: column;
-	gap: 14px;
-	overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  overflow-y: scroll;
 }
 
 .showBgImg {
-	height: 237px;
-	width: 100%;
-	position: fixed;
-	top: 0;
-	left: 0;
-	z-index: -1;
+  height: 237px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
 }
 
 .showBasicInfoContainer {
-	display: flex;
-	align-items: flex-end;
-	justify-content: space-between;
-	gap: 17px;
-	margin-top: 100px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 17px;
+  margin-top: 100px;
 
-	z-index: 2;
-	color: #fff;
-	font-family: Helvetica;
-	font-size: 10px;
-	font-style: normal;
-	font-weight: 400;
-	line-height: 10px;
+  z-index: 2;
+  color: #fff;
+  font-family: Helvetica;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 10px;
 }
 
 .showBasicInfoContainer .poster {
-	width: 120px;
-	height: 160px;
-	border-radius: 7px;
-	object-fit: cover;
-	flex-shrink: 0;
+  width: 120px;
+  height: 160px;
+  border-radius: 7px;
+  object-fit: cover;
+  flex-shrink: 0;
 }
 
 .showBasicInfoContainer .infoBox {
-	flex: 1;
+  flex: 1;
 }
 
 .showBasicInfoContainer .showName {
-	color: #fff;
-	text-align: left;
-	font-family: Helvetica;
-	font-size: 20px;
-	font-style: normal;
-	font-weight: 700;
-	line-height: 27.879px;
+  color: #fff;
+  text-align: left;
+  font-family: Helvetica;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 27.879px;
 
-	word-wrap: break-word;
-	margin-bottom: 17px;
-	flex: 1;
+  word-wrap: break-word;
+  margin-bottom: 17px;
+  flex: 1;
 }
 
 .showBasicInfoContainer .showTheater {
-	display: flex;
-	align-items: center;
-	gap: 5px;
-	margin-bottom: 7px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  margin-bottom: 7px;
 }
 
 .showBasicInfoContainer .infoBox:last-child {
-	margin-bottom: 12.5px;
+  margin-bottom: 12.5px;
 }
 
 .showCommentsBox {
-	position: fixed;
-	top: 301px;
-	left: 30px;
-	z-index: 2;
-	width: calc(100% - 60px);
-	max-height: calc(100% - 301px);
-	display: flex;
-	flex-direction: column;
-	overflow-y: scroll;
-	gap: 14px;
+  position: fixed;
+  top: 301px;
+  left: 30px;
+  z-index: 2;
+  width: calc(100% - 60px);
+  max-height: calc(100% - 301px);
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+  gap: 14px;
 }
 
 .showCommentsChangeBox {
-	display: flex;
-	flex-direction: column;
-	margin-top: 5px;
-	gap: 22px;
-	overflow-y: auto;
-	/* Enable vertical scrolling */
-	scrollbar-width: none;
+  display: flex;
+  flex-direction: column;
+  margin-top: 5px;
+  gap: 22px;
+  overflow-y: auto;
+  /* Enable vertical scrolling */
+  scrollbar-width: none;
 }
 
 .commentBox {
-	border-radius: 20px;
-	background: var(--main-Background-cold, #e9ebea);
-	flex-shrink: 0;
-	padding: 10px;
+  border-radius: 20px;
+  background: var(--main-Background-cold, #e9ebea);
+  flex-shrink: 0;
+  padding: 10px;
 }
 
 .commentImgBox {
-	display: flex;
-	gap: 10px;
-	overflow-x: scroll;
-	margin-bottom: 14px;
+  display: flex;
+  gap: 10px;
+  overflow-x: scroll;
+  margin-bottom: 14px;
 }
 
 .commentImg {
-	width: 118px;
-	height: 118px;
-	flex-shrink: 0;
-	border-radius: 5px;
-	object-fit: cover;
+  width: 118px;
+  height: 118px;
+  flex-shrink: 0;
+  border-radius: 5px;
+  object-fit: cover;
 }
 
 .commentText {
-	color: #000;
-	font-family: Helvetica;
-	font-size: 10px;
-	font-style: normal;
-	font-weight: 400;
-	line-height: 14px;
-	margin-bottom: 14px;
+  color: #000;
+  font-family: Helvetica;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 14px;
+  margin-bottom: 14px;
 }
 
 .commentStar {
-	display: flex;
-	justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 .commentDate {
-	color: #000;
-	text-align: right;
-	font-family: Helvetica;
-	font-size: 10px;
-	font-style: normal;
-	font-weight: 400;
-	line-height: 10px;
+  color: #000;
+  text-align: right;
+  font-family: Helvetica;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 10px;
 }
 
 .rotating-icon {
-	width: 20px;
-	/* 根据需要调整图标大小 */
-	height: 20px;
-	animation: rotate 1s linear infinite;
-	display: block;
-	margin: 0 auto;
+  width: 20px;
+  /* 根据需要调整图标大小 */
+  height: 20px;
+  animation: rotate 1s linear infinite;
+  display: block;
+  margin: 0 auto;
 }
 
 .end-indicator {
-	margin-top: 2px;
-	color: white;
-	font-size: 20px;
-	display: block;
-	margin: 0 auto;
+  margin-top: 2px;
+  color: white;
+  font-size: 20px;
+  display: block;
+  margin: 0 auto;
 }
 </style>
 <style>
 @keyframes rotate {
-	from {
-		transform: rotate(0deg);
-	}
+  from {
+    transform: rotate(0deg);
+  }
 
-	to {
-		transform: rotate(360deg);
-	}
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
